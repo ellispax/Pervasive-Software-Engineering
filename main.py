231ef09a319ea337f01793c4b5cde4af502ce7a3
ELLIS PAX MAPAKAMA <mapakameellis@gmail.com>
2022-01-30 18:32:15 +0700

    Adding final features and functions

--- main.py
+++ main.py
@@ -36,6 +36,8 @@ GPIO.setup(IR_in,GPIO.IN)
 GPIO.setup(IR_out,GPIO.IN)
 GPIO.setup(button1, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup(button2, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+BS1 = False
+BS2 = False
 
 while (1):
     # GPIO.output(led_green_in, False)
@@ -43,8 +45,8 @@ while (1):
     # GPIO.output(led_red_in, True)
     # GPIO.output(led_red_out, True)
 
-    state=GPIO.input(IR_in)
-    if state==False:
+    state_IN=GPIO.input(IR_in)
+    if state_IN==False:
         
         time.sleep(1)
         print("Object Detected on IN")
@@ -68,7 +70,56 @@ while (1):
             GPIO.output(led_green_in, False)
             
         else:
-            print("Please Tap if you are visitor")
+            print("Please Press Button if you are visitor")
+            if GPIO.input(button1) == 0:
+                if BS1 == False:
+                    insert(num_plate, stamp, desig)
+                    GPIO.output(led_red_in, False)
+                    set_angle(180.0)
+                    print("Gate Opened")
+                    GPIO.output(led_green_in, True)
+                    sleep(5)
+                    set_angle(0.0)
+                    update_counter_in(desig)
+                    print("Gate Closed")
+                    GPIO.output(led_green_in, False)
+                    
+
+
     else:
         time.sleep(1)
         GPIO.output(led_red_in, True)
+    
+    state_OUT = GPIO.input(IR_out)
+
+    if state_OUT == False:
+        
+        time.sleep(1)
+        print("Object Detected on out")
+        image_out = capture(get_stamp())
+        numPlate = ocr(image_out)
+
+        #num_plate = ocr(path)
+        #num_plate = 'B2743TZA'
+        #np = 'B9674S'
+        idx = get_idxout(numPlate)
+        proceed, desig = checkExt(idx)
+        if proceed == 'go':
+            GPIO.output(led_green_out, True)
+            set_angle2(180.0)
+            print("Gate Opened")
+            sleep(5)
+            update_out(idx)
+            set_angle2(0.0)
+            print("Gate Closed")
+            GPIO.output(led_green_out, False)
+            update_counter_out(desig)
+        else:
+            print('Error on Exit Check Db or use manual exit')
+        
+        
+        
+    else:
+        time.sleep(1)
+        GPIO.output(led_red_out, True)
+        
